c80c6880a9bac454aee8b049a29058c87f5ab5bd
Mamadou Mourtalla Ndjitte <mourtalladjite@outlook.fr>
2021-03-07 02:32:02 +0000

    premier comit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+const btns = document.querySelectorAll('.btn-modal');
+const modal = document.querySelector('.bloc-modal');
+const close = document.querySelector('.close');
+const index = document.querySelector('.bloc-modal img');
+
+btns.forEach(btn => {
+    btn.addEventListener(click, (e) => {
+        index.src = 'img/ep${e.target.getAttribute('data - index')}.jpg';
+        modal.classList.add('active');
+
+    })
+})
+
+modal.addEventListener(click, () => {
+    modal.classList.remove('active');
+})
